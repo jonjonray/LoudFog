aebbc16a20e1f0991d4161b7246ee9fd23226914
jonjonray <jonjonray@gmail.com>
2017-11-27 21:40:53 -0800

    fix placeholder cloud typo

--- frontend/components/placeholder_cloud.jsx
+++ frontend/components/placeholder_cloud.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 export default () => (
-  <div class="main-right-placeholder">
+  <div className="main-right-placeholder">
     <p>LOUDFOG</p>
   <img className="placeholder-cloud"
     src="https://s3-us-west-1.amazonaws.com/loudfog/placeholder-cloud.png">
