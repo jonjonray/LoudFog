9423fb010e0dd2164c1c4e8146f88b808511274e
jonjonray <jonjonray@gmail.com>
2017-11-30 19:38:03 -0800

    alter seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,21 +16,25 @@ avatars = [
 "https://s3-us-west-1.amazonaws.com/loudfog/avatar_4.jpeg",
 "https://s3-us-west-1.amazonaws.com/loudfog/avatar_5.jpeg"
           ]
-
+username = ["userBob", "meanCat", "AndresTheGreat", "hassium", "dolerite",
+            "popinjay", "SeaTriscuit", "gus_music", "wyatt", "buster"]
 
 10.times do |i|
-  name = Faker::Internet.user_name
-  email = Faker::Internet.safe_email
+  name = username[i]
+  email = "test@loudfog.com"
   User.create(username: name, password: "password", email: email, avatar_url: avatars[i])
 end
+
+songs = ["Lost Things", "Darling, The Night Is Still Young", "Light For The Road",
+"Think Of Power","Broken Chances", "Songs For Tomorrow", "When You're Smiling",
+"Midnight Inventions", "Never Let Me Go", "Walking Mind", "Stormy Money",
+"Sound Of The Evening", "Dust Of Tomorrow"]
 
 
 13.times do |i|
   user_id = User.all.sample.id
-  title = Faker::BossaNova.song
-  while title.length < 5 do
-      title = Faker::BossaNova.song
-  end
+  title = songs[i]
+
   image = File.new("app/assets/images/seed/#{i+1}.jpg")
   audio = File.new("app/assets/music/#{i+1}.mp3")
   Song.create(user_id: user_id, title: title, image: image, audio: audio)
